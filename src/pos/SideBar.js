5871a75b542a2e6366f057161815a83488642340
rheshav <rhesa.havilah@gmail.com>
2022-01-14 15:22:36 +0700

    edit

--- src/pos/SideBar.js
+++ src/pos/SideBar.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import MenuList from './MenuList';
+import Profile from './Profile';
+import Settings from './Settings';
+
 function SideBar(props) {
   return (
     <div>
       <p>SideBar</p>
+      <MenuList></MenuList>
+      <Profile></Profile>
+      <Settings></Settings>
     </div>
   );
 }
